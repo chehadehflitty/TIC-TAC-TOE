536dbe1f9077959acdfbb17d4456538b94eee034
Chehadeh Flitty <flitty.chehadeh@gmail.com>
2023-07-13 21:34:17 +0300

    creating check for winner function

--- script.js
+++ script.js
@@ -22,5 +22,26 @@ function startGame(){
   }
 }
 
+function checkWin(player){
+  const winning_index_table = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winning_index_table.length; i++) {
+    const [a, b, c] = winning_index_table[i];
+    if (cells[a].textContent === player && cells[b].textContent === player && cells[c].textContent === player) {
+        return true;
+    }
+  }
+  return false;
+}
+
 
 
